a00d8ea57868641ff2c19900a6c88cb6a96b2938
Gullbra <martingullbrandssonmoreira@hotmail.com>
2023-04-08 16:51:54 +0200

    repo set up

---
+++ README.md
@@ -0,0 +1,3 @@
+# Library-PHP
+
+Private library of notes, code snippets and applications, about/written in PHP
---
+++ bc_tutorial/bc_01_set_up_and_basics/index.php
@@ -0,0 +1,36 @@
+<?php 
+  echo"Hey World";
+  // comment!
+  // * better comments extension support
+
+  /*
+  multiline comment
+  */
+
+  // Html templates work:
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testing</title>
+  </head>
+
+  <body>
+    <p>Hey there!</p>
+    <p id="clickDisplay">0</p>
+
+    <script>
+      const clickHandler = () => {
+        const newNum = Number(document.querySelector('#clickDisplay').textContent) + 1
+        document.querySelector('#clickDisplay').textContent = newNum
+      }
+    </script>
+
+    <button onclick="clickHandler()">click me</button>
+  </body>
+</html>
